487e143a38447feea1e588fb128e50e06ec87dbb
SK <transiencymail@gmail.com>
2024-06-10 08:34:40 -0600

    message model with broadcast

--- web/app/models/message.rb
+++ web/app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
+
+  after_create_commit -> { broadcast_append_to conversation }
+
+  # Optional: broadcast updates or removals
+  after_update_commit -> { broadcast_replace_to conversation }
+  after_destroy_commit -> { broadcast_remove_to conversation }
 end
